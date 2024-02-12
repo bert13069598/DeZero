112741a439c3b60b2aa8f8bda5759a56cc0ba56a
bert13069598 <everglow83@naver.com>
2024-02-12 22:15:12 +0900

    step 2

--- steps/notice01.py
+++ steps/notice01.py
@@ -6,5 +6,22 @@ class Variable:
         self.data = data
 
 
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        return Variable(y)
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
 x = Variable(np.array(10))
-print(x.data)
+f = Square()
+y = f(x)
+print(y.data)
