8322549a21ade6d9eb231cb7f121883d284dc256
bert13069598 <everglow83@naver.com>
2024-02-12 23:09:15 +0900

    step 4 - 수치 미분

--- steps/notice01.py
+++ steps/notice01.py
@@ -26,12 +26,25 @@ class Exp(Function):
         return np.exp(x)
 
 
-A = Square()
-B = Exp()
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+f = Square()
+x = Variable(np.array(2.0))
+dy = numerical_diff(f, x)
+print(dy)
+
+
+def f(x):
+    A = Square()
+    B = Exp()
+    return A(B(A(x)))
 
 x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = A(b)
-print(y.data)
-
+dy = numerical_diff(f, x)
+print(dy)
