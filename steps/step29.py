4322d7e0a56f7b2ca7d48ff7d83bcad507e2e544
bert13069598 <everglow83@naver.com>
2024-02-17 22:37:09 +0900

    step 32 - 고차 미분 (구현)

--- steps/step29.py
+++ steps/step29.py
@@ -12,16 +12,19 @@ def f(x):
     return y
 
 
-def gx2(x):
-    return 12 * x ** 2 - 4
-
-
 x = Variable(np.array(2.0))
-y = f(x)
-y.backward(create_graph=True)
-print(x.grad)
+iters = 10
 
-gx = x.grad
-x.cleargrad()
-gx.backward()
-print(x.grad)
+for i in range(iters):
+    print(i, x)
+
+    y = f(x)
+    x.cleargrad()
+    y.backward(create_graph=True)
+
+    gx = x.grad
+    x.cleargrad()
+    gx.backward()
+    gx2 = x.grad
+
+    x.data -= gx.data / gx2.data
